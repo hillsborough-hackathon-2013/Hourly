e274a8a1b99ee45ddac59a66e52fffd7b4760892
nerdylikeme <liamgym4@gmail.com>
2013-04-13 11:26:56 -0400

    initial project check in

---
+++ src/services/Hourly.Services/App_Start/RemoveDuplicateContentAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Hourly.Services
+{
+    public class RemoveDuplicateContentAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpRequestBase request = filterContext.HttpContext.Request;
+
+            if (!filterContext.IsChildAction && !request.IsAjaxRequest())
+            {
+                var routes = RouteTable.Routes;
+                var requestContext = filterContext.RequestContext;
+                var routeData = requestContext.RouteData;
+                var dataTokens = routeData.DataTokens;
+
+                if (dataTokens["area"] == null)
+                {
+                    dataTokens.Add("area", "");
+                }
+
+                VirtualPathData vpd = routes.GetVirtualPathForArea(requestContext, routeData.Values);
+                if (vpd != null)
+                {
+                    String virtualPath = vpd.VirtualPath.ToLower();
+                    if (!string.Equals(virtualPath, request.Path))
+                    {
+                        filterContext.Result = new RedirectResult(virtualPath + request.Url.Query, true);
+                    }
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
